1824045d25d0821d6f788ec98cf070a6cb00644d
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-17 15:15:49 +0000

    numbers 0 -9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - single unit numbers
+ * Description: print out single unit base 10 numbers
+ * Return: success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i\n", n);
+	}
+	return (0);
+}
