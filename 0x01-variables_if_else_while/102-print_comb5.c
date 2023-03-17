d7f815ac157b2070f2e521437a169c5dcb6d7e3a
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-17 22:24:43 +0000

    4 digit combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - 4 digit combo
+ * Description: combine 4 digits with 4  variables
+ * Return: success
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+	a = '0';
+	while (a <= '9')
+	{b = '0';
+		while (b <= '9')
+		{
+		c = '0';
+		while(c <= '9')
+		{
+			d = '0';
+			while (d <= '9')
+			{
+			if(a <= c && b < d)
+			{
+			putchar(a);
+			putchar(b);
+			putchar(' ');
+			putchar(c);
+			putchar(d);
+			if (a == '9' && b == '8' && c == '9' && d == '9')
+                        {
+			putchar('\n');
+			}
+			else
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+			d++;
+			}
+			c++;
+                        }
+                        b++;
+                }
+                a++;
+        }
+	return (0);
+}
