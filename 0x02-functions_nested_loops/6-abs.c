26c476c2e28385b960bcecdd69205ae29ccd14ca
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-21 16:49:21 +0000

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,10 +2,11 @@
 /**
  * _abs - absolute
  * Description: prints absolute number
+ * @n: number to be checked
  * Return: returns absolute of int
  */
 
-int _abs(int n)
+int _abs (int n)
 {
 	return ( n < 0 ? 0 - n : n);
 }
