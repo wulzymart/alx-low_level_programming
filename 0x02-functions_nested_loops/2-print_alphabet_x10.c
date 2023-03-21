64a352b40f2fb2d51317996040634ca75bc650c8
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-21 15:14:18 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print a-z 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char a = 'a';
+
+	while (i < 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		i++;
+	}
+}
