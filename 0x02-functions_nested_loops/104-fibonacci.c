d4dc90e6c56ba9b381bdac86b6d3ccd7da343fe0
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-22 01:46:57 +0000

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print fibunacci numbers
+ * Description: prints 1st 98 fibunacci numbers
+ * Return: 0;
+ */
+
+int main(void)
+{
+	unsigned long a, b, c;
+	int i = 0;
+
+	a = 0;
+	b = 1;
+	while (i < 98)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu", c);
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
