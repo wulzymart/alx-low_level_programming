7298afb0c819ce95dcafc0445bd8fb1801769a97
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-23 16:56:20 +0000

    tasks 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a line diagonally a given number of times
+ * @n: width of line
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	x = 0;
+	while (x < n)
+	{
+		y = 0;
+		while (y <= x)
+		{
+			y == x ? _putchar('\\') : _putchar(' ');
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
