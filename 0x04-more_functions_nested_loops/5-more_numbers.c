4edb74f5c1270eab0b775a1a82ceb0bb1a5ae43f
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-23 15:29:23 +0000

    tasks 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 1 - 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	x = 0;
+	while (x <= 10)
+	{
+		y = 0;
+		while (y < 15)
+		{
+			if (y >= 10)
+				_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+		x++;
+	}
+}
