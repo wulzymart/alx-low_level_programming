4c5f1e0cb61ab41dd1dbf715930401718c6e2b33
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-23 12:32:18 +0000

    tasks 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
