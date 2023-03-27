073387258d87229bc4cde3dc0d03dcd5e60d4b49
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-27 16:58:20 +0000

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - count length of string
+ * @s: string to be counted address of string
+ * Return: returns the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ * Return: result
+ */
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int i = 0;
+	int minus = 0;
+
+	while (!(*(s + i) >= '0' && *(s + i) <= '9') && *(s + i))
+	{
+		if (*(s + i) == '-')
+			minus++;
+		i++;
+	}
+
+	while (*(s + i) && (*(s + i) <= '9' && *(s + i) >= '0'))
+	{
+		n = (n * 10) + (*(s + i) - '0');
+		i++;
+	}
+
+	n = minus % 2 != 0 ? 0 - n : n;
+	return (n);
+}
