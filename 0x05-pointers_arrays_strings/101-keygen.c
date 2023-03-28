6e23dc66e3d3ae8af3f6bdf5e507cfea22025204
Olawole Olawale Martins <olawalemart@gmail.com>
2023-03-28 00:18:58 +0000

    task 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - keaygen for 101-crackme
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int check = 2772;
+	int n;
+
+	srand(time(NULL));
+
+	while (check > 122)
+	{
+		n = rand() % 75;
+		putchar(n +48);
+		check -= (n + 48);
+	}
+	if (check > 0)
+		putchar(check);
+}
