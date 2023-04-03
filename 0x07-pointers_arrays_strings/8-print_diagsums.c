a088bf14ff71525f56e2554006fbc608e25b5549
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-03 15:50:07 +0000

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,20 +13,13 @@ void print_diagsums(int *a, int size)
 	long sum1, sum2;
 
 	i = 0;
+	j = size - 1;
 	sum1 = sum2 = 0;
 	while (i < size)
 	{
 		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + j - i);
 		i++;
 	}
-	j = size - 1;
-	i = 0;
-	while (j >= 0 && i < size)
-	{
-		sum2 += *(a + i * size + j);
-		j--;
-		i++;
-	}
-
 	printf("%lu, %lu\n", sum1, sum2);
 }
