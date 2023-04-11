69b6d1a4036d693af3890448f10583fc0b14d3e4
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-11 09:15:23 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *p = malloc(sizeof(char) * size);
+	char *p;
 
+	if (size == 0)
+		return (NULL);
+	p == malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
 	return (p);
