de8192936b1bf1c494e5630ac6103f0cb378deeb
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-13 18:23:21 +0000

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,8 +16,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		new = malloc(new _size);
+		new = malloc(new_size);
 		return (new);
+	}
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
