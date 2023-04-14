4450ee6ab4c9f9654f80f7a937c5750a3e7126f3
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-14 02:38:02 +0000

    task 101 update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,58 +1,41 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _strlen - gets length of a string
- * @str: string
- * Return: length of string
+ * pos_num - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-
-int _strlen(char *str)
+int pos_num(char *s)
 {
 	int i = 0;
 
-	while (str[i])
-		i++;
-	return (i);
-}
-
-/**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: number of array members
- * @size: size of member elements
- * Return: NULL if it fails and pointer to the allocated memory if it success.
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *ptr;
-	unsigned int i, s;
-
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	s = nmemb * size;
-	ptr = malloc(s);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i < s; i++)
-		ptr[i] = 0;
-	return (ptr);
-}
-
-/**
- * pos_num - checks if a string is positive number
- * @str: string to check
- * Return: 1 if true 0 if false
- */
-
-int pos_num(char *str)
-{
-	int i;
-
-	for (i = 0; str[i]; i++)
-		if (str[i] < '0' || str[i] > '9')
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
+	}
 	return (1);
+}
+
+/**
+ * _strlen - get the length of a string
+ * @s: string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -61,52 +44,48 @@ int pos_num(char *str)
  */
 void error(void)
 {
-	char *s = "error";
-
-	for (; *s; s++)
-		_putchar(*s);
-	_putchar('\n');
+	printf("Error\n");
 	exit(98);
 }
 
 /**
- * main - multiplies two positive numbers.
- * @argc: arguement count
- * @argv: arguement list
- * Return: ) if success, 98 if failure
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: argument list
+ *
+ * Return:0 (Success)
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *res;
-	int l1, l2, l, rem, n1, n2, i, j;
+	int l1, l2, l, i, rem, n1, n2, *res, j;
 
 	if (argc != 3 || !pos_num(argv[1]) || !pos_num(argv[2]))
 		error();
 	l1 = _strlen(argv[1]);
 	l2 = _strlen(argv[2]);
-	l = l1 + l2;
-	res = _calloc(l + 1, 1);
+	l = l1 + l2 + 1;
+	res = malloc(sizeof(int) * l);
 	if (res == NULL)
-		return (1);;
-	l1 = l1 - 1;
-	for (; l1 >= 0; l1--)
+		return (1);
+	for (i = 0; i < l ; i++)
+		res[i] = 0;
+	for (l1 = l1 - 1; l1 >= 0; l1--)
 	{
 		n1 = argv[1][l1] - '0';
 		rem = 0;
 		for (l2 = _strlen(argv[2]) - 1; l2 >= 0; l2--)
 		{
 			n2 = argv[2][l2] - '0';
-			rem += res[l1 + l2 + 1] + n1 * n2;
+			rem += res[l1 + l2 + 1] + (n1 * n2);
 			res[l1 + l2 + 1] = rem % 10;
 			rem /= 10;
 		}
-		res[l1 + l2 + 1] = !rem ? res[l1 + l2 + 1] :
-		res[l1 + l2 + 1] + rem;
+		if (rem > 0)
+			res[l1 + l2 + 1] += rem;
 	}
-	for (i = 0, j = 0; i < l; i++)
+	for (i = 0, j = 0; i < l - 1; i++)
 	{
-		if (res[i] > 0)
+		if (res[i])
 			j = 1;
 		if (j)
 			_putchar(res[i] + '0');
