d5551663498915535746f440c856df4a96c8a8b6
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-17 12:46:07 +0000

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a struct dog
+ *@d: pointer to struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		d->name ? printf("%s\n", d->name) : printf("(nil)\n");
+		printf("Age: ");
+		d->age ? printf("%f\n", d->age) : printf("(nil)\n");
+		printf("Owner: ");
+		d->owner ? printf("%s\n", d->owner) : printf("(nil)\n");
+	}
+}
