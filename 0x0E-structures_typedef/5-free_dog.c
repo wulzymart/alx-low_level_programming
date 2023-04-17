301e88f92932b9432cca37c6ca68f8b9fb8fe030
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-17 14:16:40 +0000

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,7 +8,12 @@
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
+	if (d != NULL)
+	{
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
 	free(d);
+	}
 }
