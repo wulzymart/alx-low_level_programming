75d4ffd8190fd2f45d32d19d6ebcf9ab2b3e9377
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-20 11:44:37 +0000

    tasks 1 update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
-			printf("%d", va_arg(args, int));
-			if (separator != NULL && i < (n - 1))
-				printf("%s", separator);
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < (n - 1))
+			printf("%s", separator);
 	}
 	putchar('\n');
 	va_end(args);
