89d0c90987f3dd2c7c24683e3b43f9e6d7e75f7e
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-05-08 21:52:17 +0100

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+ * printerr97 - print error 87
+*/
+void printerr97(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
+ * printerr98 - print error 87
+ * @str: arguement string
+*/
+void printerr98(char *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+	exit(98);
+}
+/**
+ * printerr99 - print error 87
+ * @str: arguement string
+*/
+void printerr99(char *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", str);
+	exit(99);
+}
+/**
+ * main - copies the content of a file to another file.
+ * @ac: arguement count
+ * @av: arguement vector
+ * Return: 0 if success
+*/
+int main(int ac, char **av)
+{
+	int a = 0, b = 0, d, e, fd1, fd2;
+	char buff[1024];
+
+	if (ac != 3)
+		printerr97();
+	fd1 = open(av[1], O_RDONLY);
+	if (fd1 == -1)
+		printerr98(av[1]);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		printerr99(av[2]);
+	do {
+		e = read(fd1, buff, 1024);
+		if (e == -1)
+			printerr98(av[1]);
+		d = write(fd2, buff, e);
+		if (d != e)
+			printerr99(av[2]);
+	} while (e != 0);
+	a = close(fd1);
+	b = close(fd2);
+	if (a == -1 || b == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a != 0 ? a : b);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
