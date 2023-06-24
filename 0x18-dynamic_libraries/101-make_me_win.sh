5fe3739c6ce5f1cd36beee0400e8fbc49f10d89e
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-24 01:58:36 +0100

    script edited

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 wget https://github.com/wulzymart/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libatoi.so -O /tmp/libatoi.so
-export LD_PRELOAD="/tmp/libatoi.so
-
-
+export LD_PRELOAD="/tmp/libatoi.so"
