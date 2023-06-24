5a36895c66f3c180d7ee1f3973271c1f31af74ad
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-24 01:53:09 +0100

    script edited

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO ../libat.so https://github.com/wulzymart/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libatoi.so
-export LD_PRELOAD="../libat.so"
+wget https://github.com/wulzymart/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libatoi.so -O ../libatoi.so
+export LD_PRELOAD="../libatoi.so"
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -28,3 +28,4 @@ Create a script that creates a dynamic library called liball.so from all the .c 
 
 # Task 2:
 Create a dynamic library that contains C functions that can be called from Python.
+# Task 101:
