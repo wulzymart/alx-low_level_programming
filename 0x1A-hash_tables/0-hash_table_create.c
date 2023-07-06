9385bd49aaa7e84912bfbca9c1f2f755a6f0e42e
Olawole Olawale Martins <olawalemart@gmail.com>
2023-07-06 22:55:09 +0100

    Tasks 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new = NULL;
 	unsigned long i;
 
-	if (!size)
-		return (NULL);
 	new = malloc(sizeof(hash_node_t *));
 	if (!new)
 		return (NULL);
